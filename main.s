f69c6b02f9343b18042a23085d39138642f47160
yourname <youremail@mail.com>
2019-03-10 00:08:10 +0800

    done

--- main.s
+++ main.s
@@ -11,16 +11,60 @@ _start:
 	//
 	//branch w/o link
 	//
-	b	label01
+	b	initial_1
 
-label01:
+initial_1:
 	nop
+	MOV r1,#1
+	MOV r2,#2
+	MOV r3,#3
+        MOV r4,#4
+        MOV r5,#5
+        MOV r6,#6
+	
+	b 	label_a
+label_a://observe the behavior of pop 
+	nop
+	PUSH    {r1}
+	PUSH	{R2}
+	push	{r3}
+	push	{r4}
+	PUSH	{r5}
+	push	{r6}
+	pop	{r1,r2,r3,r4,r5,r6}
+ 
+  
+	b	initial_2
+initial_2:
+        nop
+        MOV r1,#1
+        MOV r2,#2
+        MOV r3,#3
+        MOV r4,#4
+        MOV r5,#5
+        MOV r6,#6
 
-	//
-	//branch w/ link
-	//
-	bl	sleep
+        b       label_b
+label_b:
+        nop
+        push     {r1,r2,r3,r4,r5,r6}
+        Pop    {r1}
+        Pop    {R2}
+        pop    {r3}
+        pop    {r4}
+        Pop    {r5}
+        pop    {r6}
 
+        push     {r1,r2,r3,r4,r5,r6}
+        Pop    {r6}
+        Pop    {r5}
+        pop    {r4}
+        pop    {r3}
+        Pop    {r2}
+        pop    {r1}
+
+	
+	b	sleep       
 sleep:
 	nop
-	b	.
+	bl	_start
